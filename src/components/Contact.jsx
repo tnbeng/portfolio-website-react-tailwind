0ee390929a461d4b253f791ffba38ed18cc71399
Tarak Nath Bar <btarak398@gmail.com>
2024-09-19 01:39:24 +0530

    2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaEnvelope, FaMapMarkedAlt, FaPhone } from 'react-icons/fa'
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const baseUrl=process.env.REACT_APP_BASE_URL;
+    try {
+      const response = await fetch(`${baseUrl}/send`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        alert('Message sent successfully');
+        // Optionally, clear the form after submission
+        // setFormData({ name: '', email: '', message: '' });
+      } else {
+        alert('Failed to send the message');
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+      alert('An error occurred while sending the message');
+    }
+  };
+
   return (
     <div className="bg-black text-white py-20" id="contact">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         <h2 className="text-4xl font-bold text-center mb-12">Contact Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
           <div className="flex-1">
-            <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r 
-            from-green-400 to-blue-500 mb-4'>Let's Talk</h3>
-            <p>I'm open to discussing web development prjectgs or partnership opportunities.</p>
+            <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 mb-4'>
+              Let's Talk
+            </h3>
+            <p>I'm open to discussing web development projects or partnership opportunities.</p>
             <div className='mb-4 mt-8'>
               <FaEnvelope className='inline-block text-green-400 mr-2'></FaEnvelope>
               <a href="mailto:btarak398@gmail.com" className='hover:underline'>
@@ -27,37 +67,48 @@ const Contact = () => {
             </div>
           </div>
           <div className='flex-1 w-full'>
-            <form className='space-y-4'>
+            <form onSubmit={handleSubmit} className='space-y-4'>
               <div>
                 <label htmlFor="name" className='block mb-2'>Your Name</label>
-                <input type="text"
-                  className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none
-                    focus:border-green-400'
-                  placeholder='Enter You Name' required/>
+                <input 
+                  type="text" 
+                  name="name" 
+                  value={formData.name}
+                  onChange={handleChange}
+                  className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none focus:border-green-400'
+                  placeholder='Enter Your Name' 
+                  required />
               </div>
               <div>
-                <label htmlFor="emial" className='block mb-2'>Email</label>
-                <input type="text"
-                  className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none
-                    focus:border-green-400'
-                  placeholder='Enter You Email' required/>
+                <label htmlFor="email" className='block mb-2'>Email</label>
+                <input 
+                  type="email" 
+                  name="email" 
+                  value={formData.email}
+                  onChange={handleChange}
+                  className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none focus:border-green-400'
+                  placeholder='Enter Your Email' 
+                  required />
               </div>
               <div>
                 <label htmlFor="message" className='block mb-2'>Message</label>
-                <textarea type="text"
-                  className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none
-                    focus:border-green-400'
+                <textarea 
+                  name="message" 
+                  value={formData.message}
+                  onChange={handleChange}
+                  className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none focus:border-green-400'
                   rows="5"
-                  placeholder='Enter You Message' />
+                  placeholder='Enter Your Message' />
               </div>
-              <button onClick={()=>{alert("Sorry! Please send me on this email btarak398@gmail.com")}}className='bg-gradient-to-r from-green-400 to-blue-500 text-white  md:inline
-            transform transition-transform duration-300 hover:scale-105 px-8 py-2 rounded-full'>Send</button>
+              <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white md:inline transform transition-transform duration-300 hover:scale-105 px-8 py-2 rounded-full'>
+                Send
+              </button>
             </form>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
